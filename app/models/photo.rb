7466e327a9210aa1eb18d8516707cc6b921828c8
Drew Kario <kario@apostrophed.com>
2016-01-19 18:15:00 -0500

    Added image display to project

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,11 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :image_content_size, :image_content_type, :image_file_name, :image_updated_at, :name
+  attr_accessible :image_content_size, :image_content_type, :image_file_name, :image_updated_at, :name, :image
   
   has_attached_file :image,
 	:styles => { :medium => '600x400>', :small => '180x180#', :search_results => '136x136#', :large => 'x572', :thumbnail => '50x50#' },
 	:default_style => :medium
+	
+  validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'
 	
   
 end
--- db/migrate/20160119221851_create_photos.rb
+++ db/migrate/20160119221851_create_photos.rb
@@ -2,7 +2,7 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
       t.string :image_content_type
-      t.integer :image_content_size
+      t.integer :image_file_size
       t.string :image_file_name
       t.string :image_updated_at
       t.string :name
