3f8cb84772977a262f41c2e06cf2e2144d7e62c7
Drew Kario <kario@apostrophed.com>
2016-01-19 17:30:41 -0500

    added simple form and paperclip

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,9 @@
 class Photo < ActiveRecord::Base
   attr_accessible :image_content_size, :image_content_type, :image_file_name, :image_updated_at, :name
+  
+  has_attached_file :image,
+	:styles => { :medium => '600x400>', :small => '180x180#', :search_results => '136x136#', :large => 'x572', :thumbnail => '50x50#' },
+	:default_style => :medium
+	
+  
 end
