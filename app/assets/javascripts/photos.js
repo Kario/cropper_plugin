f091c099f61f89cc60aaf78a87381c4eb22566a2
Drew Kario <kario@apostrophed.com>
2016-07-19 01:26:38 -0400

    Added inputs and form for cropping with auto filled values

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -8,8 +8,13 @@ $(document).ready(function() {
   cropper = new Cropper(image, {
     aspectRatio: 16 / 9,
     crop: function(e) {
-      console.log('X: ' + e.detail.x + ' Y: ' + e.detail.y + ' Width: ' + e.detail.width + ' Height: ' + e.detail.height + ' ScaleX: ' + e.detail.scaleX + ' ScaleY: ' + e.detail.scaleY );
-      console.log(e);
+      //console.log('X: ' + e.detail.x + ' Y: ' + e.detail.y + ' Width: ' + e.detail.width + ' Height: ' + e.detail.height + ' ScaleX: ' + e.detail.scaleX + ' ScaleY: ' + e.detail.scaleY );
+      //console.log(e);
+      $('#photo_crop_x').val(e.detail.x);
+      $('#photo_crop_y').val(e.detail.y);
+      $('#photo_crop_h').val(e.detail.height);
+      $('#photo_crop_w').val(e.detail.width);
+      
     },
     zoomable: false
   });
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
   attr_accessible :image_content_size, :image_content_type, :image_file_name, :image_updated_at, :name, :image
   
   has_attached_file :image,
-	:styles => { :medium => '600x400>', :small => '180x180#', :search_results => '136x136#', :large => 'x572', :thumbnail => '50x50#' },
+	:styles => { :medium => '600x400>', :small => '180x180#', :search_results => '136x136#', :large => 'x572', :thumbnail => '50x50#', :cropped => '400x225' },
     :processors => [:cropper],
 	:default_style => :medium
 	
