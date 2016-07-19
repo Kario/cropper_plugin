a14b2bfeb5e79fd75cf3098a3ba7d5de9b296896
Drew Kario <kario@apostrophed.com>
2016-07-19 01:04:00 -0400

    Added code in photo for handling cropping with custom processor

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,12 +1,17 @@
+var image, cropper;
+
+
 $(document).ready(function() {
   
   //Basic usage copied from website 
-  var image = document.getElementById('img_crop');
-  var cropper = new Cropper(image, {
+  image = document.getElementById('img_crop');
+  cropper = new Cropper(image, {
     aspectRatio: 16 / 9,
     crop: function(e) {
       console.log('X: ' + e.detail.x + ' Y: ' + e.detail.y + ' Width: ' + e.detail.width + ' Height: ' + e.detail.height + ' ScaleX: ' + e.detail.scaleX + ' ScaleY: ' + e.detail.scaleY );
-    }
+      console.log(e);
+    },
+    zoomable: false
   });
 
 });
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,9 +3,22 @@ class Photo < ActiveRecord::Base
   
   has_attached_file :image,
 	:styles => { :medium => '600x400>', :small => '180x180#', :search_results => '136x136#', :large => 'x572', :thumbnail => '50x50#' },
+    :processors => [:cropper],
 	:default_style => :medium
 	
   validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'
-	
+
+  after_update :reprocess_avatar, :if => :cropping?
   
+  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h, :crop_style
+  
+  def cropping?
+    !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank? && !crop_style.blank?
+  end
+  
+  private
+  
+  def reprocess_avatar
+    avatar.reprocess! crop_style.parameterize.underscore.to_sym
+  end
 end
